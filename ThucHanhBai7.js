e7d7ca83e87b2f21b14aab909cbca26d1cae4697
ngnganh2207 <ngocanhnguyen2207@gmail.com>
2021-03-09 18:39:22 +0700

    THBai7Sudungtoantu

---
+++ ThucHanhBai7.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("Enter/Nhập the Width");
+inputHeight=prompt("Enter/Nhập the Height");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let Dientich=width*height;
+document.write("Dientich is=" + Dientich);
